83953052b52a8f51eed73381ac2678535d69a0a9
Harsh Patel <Patelharsh241999@gmail.com>
2023-12-26 17:11:03 +0530

    * Updated Seach value to not replaces serach value.

--- src/hooks/useSearchValue.ts
+++ src/hooks/useSearchValue.ts
@@ -1,13 +1,12 @@
 const useSearchValue = (str1: string, str2: string) => {
-    let match = str2.toLowerCase().indexOf(str1.toLowerCase());
-    if (match !== -1) {
-      let wrappedStr = `<span>${str1}</span>`;
-      let result =
-        str2.slice(0, match) + wrappedStr + str2.slice(match + str1.length);
-      return result;
-    } else {
-      return str2;
-    }
-  };
-  export default useSearchValue;
-  
+  let match = str2.toLowerCase().indexOf(str1.toLowerCase());
+  if (match !== -1) {
+    const wrappedStr1 = str2.slice(match, match + str1.length);
+    let wrappedStr = `<span>${wrappedStr1}</span>`;
+    let result = str2.slice(0, match) + wrappedStr + str2.slice(match + str1.length);
+    return result;
+  } else {
+    return str2;
+  }
+};
+export default useSearchValue;
